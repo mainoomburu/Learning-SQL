719c6fdcf9bf49f2a7d65c1f2466460f498f3279
Mr. Mburu <mainoomburu@gmail.com>
2026-09-04 12:42:13 +0300

    Add files via upload

---
+++ 15. Triggers and Events Tutorial.sql
@@ -0,0 +1,61 @@
+-- Triggers --
+-- in this instance, when someone data is put in the salary table, we want to automatically update the demographics table --
+select*
+from parks_and_recreation.employee_demographics;
+
+select*
+from parks_and_recreation.employee_salary;
+
+-- It is best practice to check if the trigger you want to create exists --
+-- If it does drop it before creating --
+drop trigger if exists parks_and_recreation.insert_employee;
+
+delimiter $$
+create trigger insert_employee
+	after insert on parks_and_recreation.employee_salary
+    for each row
+begin
+	insert into parks_and_recreation.employee_demographics(employee_id, first_name, last_name)
+    values (new.employee_id, new.first_name, new.last_name);
+end $$
+delimiter ;
+
+insert into parks_and_recreation.employee_salary(employee_id, first_name, last_name, occupation, salary, dept_id)
+values(15, 'Makin', 'Kenia', 'Consultant', 72000, Null);
+
+
+drop trigger if exists parks_and_recreation.remove_employee;
+
+delimiter $$
+create trigger remove_employee
+	after insert on parks_and_recreation.employee_demographics
+	for each row
+begin
+	insert into parks_and_recreation.employee_salary(employee_id, first_name, last_name)
+    values(new.employee_id, new.first_name, new.last_name);
+end $$
+delimiter ;
+
+insert into parks_and_recreation.employee_demographics(employee_id, first_name, last_name, age, gender, birth_date)
+values(16, 'Kimmy', 'Antonelli', 19, 'Male', Null);
+
+
+-- Event --
+-- Takes place when scheduled --
+drop event if exists delete_null;
+
+delimiter $$
+create event delete_null
+on schedule every 30 second
+do
+begin
+	delete
+    from parks_and_recreation.employee_demographics
+    where birth_date is null and age is null;
+end $$
+delimiter ;
+
+
+
+
+
